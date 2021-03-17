342fcdbf25234b15840c7d079abfef75a84fd691
Adam Page <adampage1993@gmail.com>
2021-03-17 20:14:26 +0000

    add falling tetrominos

--- tetris.js
+++ tetris.js
@@ -1,10 +1,5 @@
-const container = document.getElementById('board');
-for (let i = 0; i < 200; i++) {
-  container.innerHTML += '<div>' + '</div>';
-}
-
+// Creating the game board
 const board = Array.from(document.querySelectorAll('#board div'));
-
 const width = 10;
 
 const iTetromino = [
@@ -68,6 +63,8 @@ const theTetrominos = [
 
 let currentPosition = 4;
 let currentRotation = 0;
+
+// Randomly select a tetromino
 let random = Math.floor(Math.random() * theTetrominos.length);
 let current = theTetrominos[random][currentRotation];
 
@@ -81,4 +78,23 @@ function undraw() {
   current.forEach(index => {
     board[currentPosition + index].classList.remove('tetromino');
   });
+}
+
+timerId = setInterval(moveDown, 100);
+
+function moveDown() {
+  undraw();
+  currentPosition += width;
+  draw();
+  stop();
+}
+
+function stop() {
+  if (current.some(index => board[currentPosition + index + width].classList.contains('taken'))) {
+    current.forEach(index => board[currentPosition + index].classList.add('taken'));
+    random = Math.floor(Math.random() * theTetrominos.length);
+    current = theTetrominos[random][currentRotation];
+    currentPosition = 4;
+    draw();
+  }
 }
